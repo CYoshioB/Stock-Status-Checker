504c12386de56559a8851aaa361ee95701070508
Colin Bender <cyoshiob@gmail.com>
2025-05-27 12:19:39 -0700

    Runs in <5 sec now

--- HTML_grabber_test.py
+++ HTML_grabber_test.py
@@ -6,18 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-url_list = [
-    "https://gundamplacestore.com/collections/real-grade/products/1-144-rg-gundam-astray-red-frame",    #0
-    "https://gundamplacestore.com/collections/real-grade/products/rg-32-rx-93-nu-gundam",               #1
-    "https://usagundamstore.com/collections/rg/products/rg-1-144-16-msm-07s-zgok-chars-custom",         #2
-    "https://usagundamstore.com/collections/rg/products/rg-1-144-32-rx-93-nu-gundam",                   #3
-    "https://gundamplanet.com/collections/gundam-9/products/rg-msn-02-zeong",                           #4
-    "https://gundamplanet.com/collections/gundam-9/products/rg-rx-93-nu-gundam",                        #5
-    "https://newtype.us/p/U1QAHfKuXlryFcimMpSJ/h/rg-36-rx-93-hi-nu-gundam",                             #6
-    "https://newtype.us/p/ORUrU23xYDLWOgNOm3R1/h/rg-37-god-gundam"                                      #7
-]
-#page_url = url_list[6]
-
 service = Service()
 options = Options()
 
@@ -27,21 +15,19 @@ options.add_argument('-disable-dev-shm-usage')
 
 
 driver = webdriver.Firefox(service=service, options=options)
-wait_time = 3
+wait_time = 2
+
+page_url = input("Please provide a url to check: ")
 
 try:
-    page_url = input("Please provide a url to check: ")
     driver.get(page_url)
-
-    productName_elem = driver.find_element(By.XPATH, "//meta[@property='og:title']")
-    siteName_elem = driver.find_element(By.XPATH, "//meta[@property='og:url']")
 
 except Exception as e:
     print("HTML element not found!")
 
 else:
-    productName = productName_elem.get_attribute("content")
-    siteName = siteName_elem.get_attribute("content")
+    productName = driver.find_element(By.XPATH, "//meta[@property='og:title']").get_attribute("content")
+    siteName = driver.find_element(By.XPATH, "//meta[@property='og:url']").get_attribute("content")
     if "newtype" in page_url:
         storeName = "Newtype"
     else:
@@ -53,7 +39,6 @@ else:
 
     WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
     time.sleep(wait_time)
-    driver.set_window_size(1920, 1500)
 
     page_source = driver.page_source
     add_to_count = page_source.lower().count("add to ")
