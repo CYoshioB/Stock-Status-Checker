1807d0602c456bd13d2195e7d79e8ddf42ced298
Colin Bender <cyoshiob@gmail.com>
2025-05-27 12:27:25 -0700

    Added comments for clarity

--- HTML_grabber_test.py
+++ HTML_grabber_test.py
@@ -13,7 +13,6 @@ options.add_argument('-headless')
 options.add_argument('-no-sandbox')
 options.add_argument('-disable-dev-shm-usage')
 
-
 driver = webdriver.Firefox(service=service, options=options)
 wait_time = 2
 
@@ -26,24 +25,26 @@ except Exception as e:
     print("HTML element not found!")
 
 else:
+    # Retrieve Store name, Product name, and URL from corresponding HTML metatdata tags (if present)
     productName = driver.find_element(By.XPATH, "//meta[@property='og:title']").get_attribute("content")
     siteName = driver.find_element(By.XPATH, "//meta[@property='og:url']").get_attribute("content")
     if "newtype" in page_url:
         storeName = "Newtype"
     else:
         storeName = driver.find_element(By.XPATH, "//meta[@property='og:site_name']").get_attribute("content")
-
+    # Print store & product names while waiting for next step
     print("Checking for stock of:")
     print(storeName)
     print(productName)
-
+    # Wait until HTML source body is loaded, then ensure additional time for entire page source to load
     WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
     time.sleep(wait_time)
-
+    # Convert page source to a parseable HTML string
+    # Then count the number of occurrences of "Add to Cart/Bag" & "In Stock"
     page_source = driver.page_source
     add_to_count = page_source.lower().count("add to ")
     in_stock_count = page_source.lower().count("in stock")
-
+    # If either occurrence surpasses the threshold, tell the user it's in stock with a link back to the product
     if add_to_count > 5 or in_stock_count > 2:
         print("\nGundam kit is now in stock!")
         print("Get it here:", siteName)
@@ -51,4 +52,4 @@ else:
         print("\nGundam kit is not in stock...")
 
 finally:
-    driver.quit()
+    driver.quit()       # Turn off Selenium driver once done
